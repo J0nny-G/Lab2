61b56758c2505039923c7f4bc1b2fcfc6b40c0ae
J0nny-G <128502707+J0nny-G@users.noreply.github.com>
2024-03-24 17:48:22 +0000

    ESTRUTURA VIEWS, LÓGICA DENTRO DOS FRAGMENTS

--- app/src/main/java/com/example/notes_cm/AT1.kt
+++ app/src/main/java/com/example/notes_cm/AT1.kt
@@ -14,7 +14,6 @@ class AT1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_at1)
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
